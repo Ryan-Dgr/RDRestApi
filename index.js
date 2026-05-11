c276ffda8cf3f82716c7cc96f3c501bf26b9fa05
Ryan-Dgr <r1034704@student.vives.be>
2026-05-11 18:07:21 +0200

    get id

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+// in memory moet in mongodb
 const workouts = [
   { id: 1, title: "Push Day", category: "strength", durationMinutes: 75 },
   { id: 2, title: "Pull Day", category: "strength", durationMinutes: 75 },
   { id: 3, title: "Leg Day", category: "strength", durationMinutes: 75 },
-  { id: 3, title: "Cardio", category: "cardio", durationMinutes: 30 },
+  { id: 4, title: "Cardio", category: "cardio", durationMinutes: 30 },
 ];
 
 // Define a route
@@ -15,8 +16,19 @@ app.get("/", (req, res) => {
   res.send("Workout API");
 });
 
+// get alle workouts
 app.get("/api/workouts", (req, res) => {
   res.send(workouts);
+});
+
+// get workout met id
+app.get("/api/workouts/:id", (req, res) => {
+  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
+
+  if (!workout) {
+    return res.status(404).send("workout niet gevonden");
+  }
+  res.send(workout);
 });
 
 // Start the server
