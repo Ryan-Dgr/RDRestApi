d9989ba0e0e5c38cc8bbac94932b9dc8b0a44661
Ryan-Dgr <r1034704@student.vives.be>
2026-05-11 17:58:14 +0200

    workout api

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const workouts = [
+  { id: 1, title: "Push Day", category: "strength", durationMinutes: 75 },
+  { id: 2, title: "Pull Day", category: "strength", durationMinutes: 75 },
+  { id: 3, title: "Leg Day", category: "strength", durationMinutes: 75 },
+  { id: 3, title: "Cardio", category: "cardio", durationMinutes: 30 },
+];
+
 // Define a route
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Workout API");
+});
+
+app.get("/api/workouts", (req, res) => {
+  res.send(workouts);
 });
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Listening on port 3000...');
+  console.log("Listening on port 3000...");
 });
