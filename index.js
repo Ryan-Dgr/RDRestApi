d6f2cec523ac94afbf6c40b1186e996f65e3a789
Ryan-Dgr <r1034704@student.vives.be>
2026-05-13 10:25:13 +0200

    route appart, config en secrets

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 
 const express = require("express");
-const Joi = require("joi");
 const morgan = require("morgan");
 const helmet = require("helmet");
-
+const config = require("config");
 const app = express();
+const workouts = require("./routes/workouts");
 
 app.use(express.json());
+
+console.log("Application Name:", config.get("name"));
 
 // @ts-ignore
 app.use(helmet());
@@ -17,98 +19,12 @@ if (app.get("env") === "development") {
   console.log("Morgan enabled...");
 }
 
-// in memory moet in mongodb
-const workouts = [
-  { id: 1, title: "Push Day", category: "strength", durationMinutes: 75 },
-  { id: 2, title: "Pull Day", category: "strength", durationMinutes: 75 },
-  { id: 3, title: "Leg Day", category: "strength", durationMinutes: 75 },
-  { id: 4, title: "Cardio", category: "cardio", durationMinutes: 30 },
-];
-
-// @ts-ignore
-function validateWorkout(workout) {
-  const schema = Joi.object({
-    title: Joi.string().min(3).required(),
-    category: Joi.string().valid("strength", "cardio").required(),
-    durationMinutes: Joi.number().integer().min(1).required(),
-  });
-
-  return schema.validate(workout);
-}
-
 // Define a route
 app.get("/", (req, res) => {
   res.send("Workout API");
 });
 
-// get alle workouts
-app.get("/api/workouts", (req, res) => {
-  res.send(workouts);
-});
-
-// get workout met id
-app.get("/api/workouts/:id", (req, res) => {
-  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
-
-  if (!workout) {
-    return res.status(404).send("workout niet gevonden");
-  }
-  res.send(workout);
-});
-
-// post workout
-app.post("/api/workouts", (req, res) => {
-  const result = validateWorkout(req.body);
-
-  if (result.error) {
-    return res.status(400).send(result.error.details[0].message);
-  }
-
-  const workout = {
-    id: workouts.length + 1,
-    title: req.body.title,
-    category: req.body.category,
-    durationMinutes: req.body.durationMinutes,
-  };
-
-  workouts.push(workout);
-  res.status(201).send(workout);
-});
-
-// update workout
-app.put("/api/workouts/:id", (req, res) => {
-  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
-
-  if (!workout) {
-    return res.status(404).send("workout niet gevonden");
-  }
-
-  const result = validateWorkout(req.body);
-
-  if (result.error) {
-    return res.status(400).send(result.error.details[0].message);
-  }
-
-  workout.title = req.body.title;
-  workout.category = req.body.category;
-  workout.durationMinutes = req.body.durationMinutes;
-
-  res.send(workout);
-});
-
-// delete workout
-app.delete("/api/workouts/:id", (req, res) => {
-  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
-
-  if (!workout) {
-    return res.status(404).send("workout niet gevonden");
-  }
-
-  const index = workouts.indexOf(workout);
-  workouts.splice(index, 1);
-
-  res.send(workout);
-});
+app.use("/api/workouts", workouts);
 
 const port = process.env.PORT || 3000;
 
---
+++ routes/workouts.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const Joi = require("joi");
+
+const router = express.Router();
+
+// in memory moet in mongodb
+const workouts = [
+  { id: 1, title: "Push Day", category: "strength", durationMinutes: 75 },
+  { id: 2, title: "Pull Day", category: "strength", durationMinutes: 75 },
+  { id: 3, title: "Leg Day", category: "strength", durationMinutes: 75 },
+  { id: 4, title: "Cardio", category: "cardio", durationMinutes: 30 },
+];
+
+// @ts-ignore
+function validateWorkout(workout) {
+  const schema = Joi.object({
+    title: Joi.string().min(3).required(),
+    category: Joi.string().valid("strength", "cardio").required(),
+    durationMinutes: Joi.number().integer().min(1).required(),
+  });
+
+  return schema.validate(workout);
+}
+
+// get alle workouts
+router.get("/api/workouts", (req, res) => {
+  res.send(workouts);
+});
+
+// get workout met id
+router.get("/api/workouts/:id", (req, res) => {
+  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
+
+  if (!workout) {
+    return res.status(404).send("workout niet gevonden");
+  }
+  res.send(workout);
+});
+
+// post workout
+router.post("/api/workouts", (req, res) => {
+  const result = validateWorkout(req.body);
+
+  if (result.error) {
+    return res.status(400).send(result.error.details[0].message);
+  }
+
+  const workout = {
+    id: workouts.length + 1,
+    title: req.body.title,
+    category: req.body.category,
+    durationMinutes: req.body.durationMinutes,
+  };
+
+  workouts.push(workout);
+  res.status(201).send(workout);
+});
+
+// update workout
+router.put("/api/workouts/:id", (req, res) => {
+  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
+
+  if (!workout) {
+    return res.status(404).send("workout niet gevonden");
+  }
+
+  const result = validateWorkout(req.body);
+
+  if (result.error) {
+    return res.status(400).send(result.error.details[0].message);
+  }
+
+  workout.title = req.body.title;
+  workout.category = req.body.category;
+  workout.durationMinutes = req.body.durationMinutes;
+
+  res.send(workout);
+});
+
+// delete workout
+router.delete("/api/workouts/:id", (req, res) => {
+  const workout = workouts.find((w) => w.id === parseInt(req.params.id));
+
+  if (!workout) {
+    return res.status(404).send("workout niet gevonden");
+  }
+
+  const index = workouts.indexOf(workout);
+  workouts.splice(index, 1);
+
+  res.send(workout);
+});
+
+module.exports = router;
