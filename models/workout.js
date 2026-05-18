1c28aea14dd5e0db459566c200495a3808a26b6a
Ryan-Dgr <r1034704@student.vives.be>
2026-05-18 20:02:16 +0200

    workout model simpeler. enkel lijst van exercise ids

--- models/workout.js
+++ models/workout.js
@@ -1,43 +1,5 @@
 const mongoose = require("mongoose");
 
-const workoutSetSchema = new mongoose.Schema({
-  reps: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 100,
-  },
-  kg: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 500,
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ["warmup", "normal", "dropset"],
-  },
-});
-
-const workoutExerciseSchema = new mongoose.Schema({
-  exercise: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Exercise",
-    required: true,
-  },
-  sets: {
-    type: [workoutSetSchema],
-    required: true,
-    validate: {
-      validator: function (sets) {
-        return sets.length > 0;
-      },
-      message: "A workout exercise must have at least one set.",
-    },
-  },
-});
-
 const workoutSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -55,21 +17,15 @@ const workoutSchema = new mongoose.Schema({
     required: true,
     min: 1,
     max: 300,
-    validate: {
-      // @ type is om geef foutmelding te krijgen
-      validator: function (/** @type {number} */ value) {
-        if (this.category === "cardio") {
-          return value <= 180;
-        }
-
-        return true;
-      },
-      message: "Cardio workouts cannot be longer than 180 minutes.",
-    },
   },
 
   exercises: {
-    type: [workoutExerciseSchema],
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Exercise",
+      },
+    ],
     default: [],
   },
 
